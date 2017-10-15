3418e4b838cda6afbcfb96550586beb5d68405d4
galinovivan <9225913@gmail.com>
2017-10-15 13:16:31 +0300

    s

--- themes/zu/assets/js/dist/bundle.js
+++ themes/zu/assets/js/dist/bundle.js
@@ -22534,15 +22534,16 @@ exports.default = parseDocumentRelations;
         const projectFilterItems = $('.project_filter').find('a');
         const defaultFilter = $('.default_filter');
         if (url.indexOf('filter') != -1) {
-            defaultFilter.toggleClass('active');
-        } else {
+
             projectFilterItems.each(function (key, value) {
                 const currNom = $(this).attr('data-filter');
                 if (url.indexOf(currNom) != -1) {
                     $(this).toggleClass('active');
                 }
             });
-        };
+        } else {
+            defaultFilter.toggleClass('active');
+        }
     });
 })(jQuery);
 
--- themes/zu/assets/js/src/project-filter.js
+++ themes/zu/assets/js/src/project-filter.js
@@ -6,14 +6,15 @@ import fragment from './helpers/url';
         const projectFilterItems = $('.project_filter').find('a');
         const defaultFilter = $('.default_filter');
         if (url.indexOf('filter') != -1) {
-            defaultFilter.toggleClass('active');
-        } else {
+           
         projectFilterItems.each(function(key, value) {
             const currNom = $(this).attr('data-filter');
             if (url.indexOf(currNom) != -1) {
                 $(this).toggleClass('active');
             }
         })
-    };
+    } else {
+        defaultFilter.toggleClass('active');
+    }
     })
 })(jQuery);
